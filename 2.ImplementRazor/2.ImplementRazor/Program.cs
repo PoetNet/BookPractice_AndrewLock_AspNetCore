ed12d25017249ce6c975583250ebe36256b5a85d
marfutin <130133417+marfutin@users.noreply.github.com>
2023-10-17 19:48:41 +0300

    Add EndpointMiddleware

--- 2.ImplementRazor/2.ImplementRazor/Program.cs
+++ 2.ImplementRazor/2.ImplementRazor/Program.cs
@@ -6,6 +6,11 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapRazorPages();
+    // endpoints.MapHealthChecks("/healthz");
+    endpoints.MapGet("/test", async context =>
+    {
+        await context.Response.WriteAsync("<h1>Hello everybody from tests!</h1>");
+    });
 });
 
 app.Run();
