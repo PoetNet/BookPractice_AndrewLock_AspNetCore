b33b603861203d5e5f8ec2a28d3f8a839ade99e3
r.marfutin <r.marfutin@sh.dunice.net>
2023-11-12 18:34:45 +0300

    Add listing 7.11 Sidebar

--- 3.RazorTemplates/Pages/ToDoItemModel.cshtml.cs
+++ 3.RazorTemplates/Pages/ToDoItemModel.cshtml.cs
@@ -6,11 +6,11 @@ public class ToDoItemModel : PageModel
 {
     public List<string> Tasks { get; set; }
     public string Title { get; set; }
-    public bool IsComplete { get; set; } = true;
+    public bool IsComplete { get; set; } = false;
     
     public void OnGet()
     {
         Title = "Tasks for today";
-        Tasks = new List<string> { "Buy milk", "Buy eggs", "Buy bread" };
+        Tasks = new List<string> { "Buy milk", "Buy eggs", "Buy bread", "<strong>The following tasks remain:</strong>" };
     }
 }
